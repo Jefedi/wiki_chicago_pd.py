aea815c798e10785868ad08d86ca08805a6d017e
Jefedi <92695972+Jefedi@users.noreply.github.com>
2022-08-17 13:54:51 +0200

    Create chicago_pd.py

---
+++ chicago_pd.py
@@ -0,0 +1,141 @@
+import os
+from os import * 
+from tqdm import tqdm
+
+# information_personage_chicago_pd
+
+
+def main():
+
+
+    information_personage_chicago_pd = input(("\t Quelle est votre recherche pour votre personnage :")) # sasi du nom depersonage  
+
+
+    for i in tqdm(range(100000000)): # Range = durée de chargement
+            pass
+
+
+    
+    if information_personage_chicago_pd == ("Hank Voight"): # information du personage Hank Voight
+        print ("\t Henry Hank Voight est un personnage fictif de la série télévisée Chicago PD. Il est apparu à titre récurrent dans Chicago Fire et en tant que personnage principal dans Chicago PD, en tant que chef de l'unité de renseignement du département de police de Chicago, qui opère à partir du 21e Arrondissement")
+        
+
+    elif information_personage_chicago_pd == ("Erin Lindsay"): # information du personage Erin Lindsay
+        print ("\t Erin Lindsay est un personnage fictif de la franchise NBC de Chicago TV, en tant que personnage principal de Chicago P.D. et un personnage récurrent dans Law & Order: Special Victims Unit, Chicago Fire et Chicago Med.")
+        
+
+    elif information_personage_chicago_pd == ("Jay Halstead"): # information du personage Jay Halstead
+        print ("\t Jay Halstead est un flic de talent et un tireur d'élite qualifié. Il mène ses enquêtes en respectant les règles et n'approuve pas toujours les méthodes de Voight. Jay est marqué par le meurtre d'un enfant qu'il connaissait bien et il fera tout pour lui rendre justice et aider sa famille à faire le deuil.")
+        
+
+    elif information_personage_chicago_pd == ("Kim Burgess"): # information du personage Kim Burgess
+        print ("\t Kimberly Kim Burgess est un personnage fictif de la police procédurale américaine Chicago P.D., joué par Marina Squerciati. Elle a fait sa première apparition dans l'épisode pilote Stepping Stone, qui a été diffusé sur NBC le 8 janvier 2014.")
+          
+
+    elif information_personage_chicago_pd == ("Adam Ruzek"): # information du personage Adam Ruzek 
+        print ("\t Adam Ruzek est cadet de la police recrutés par Olinsky pour faire un travail d'infiltration. Il est souvent décrit comme un amateur zélé qui plonge la tête la première dans une situation. Toutefois, il a prouvé à maintes reprises que l'écoute de ses instincts plutôt que ses supérieurs peut sauver des vies.")
+         
+
+
+
+    elif information_personage_chicago_pd == ("Hailey Upton"): # information du personage Hailey Upton 
+        print ("\t Hailey Upton rejoint les renseignements dans l'épisode 4.21 intitulé Fagin. Elle travaille aux vols et homicides et elle doit faire équipe avec les renseignements pour résoudre une affaire. Elle ne va pas se laisser faire et montrer directement à Voight qu'elle connait son métier.")
+          
+
+
+
+    elif information_personage_chicago_pd == ("Alvin Olinsky"): # information du personage Alvin Olinsky
+        print ("\t Aucune information")
+         
+
+
+    elif information_personage_chicago_pd == ("Antonio Dawson"): # information du personage Antonio Dawson
+        print("\t Antonio Dawson, interprété par Jon Seda, est un personnage fictif de la franchise de Chicago et est un personnage principal de Chicago P.D. et Justice de Chicago. Il a été présenté comme un personnage récurrent dans Chicago Fire avant le spin-off Chicago P.D. a été créé.")
+         
+
+
+
+    elif information_personage_chicago_pd == ("Kevin Atwater"): # information du personage Kevin Atwater
+        print ("\t Aucune information")
+         
+
+
+
+    elif information_personage_chicago_pd == ("Katherine Brennan"): # information du personage Katherine Brennan
+        print ("\t Aucune information")
+         
+
+
+
+    elif information_personage_chicago_pd == ("Vanessa Rojas"): # information du personage Vanessa Rojas
+        print ("\t Vanessa Rojas est un officier sous-couverture quand elle rencontre les Renseignements.")
+         
+
+
+
+    elif information_personage_chicago_pd == ("Trudy Platt"): # information du personage Trudy Platt
+        print ("\t Trudy Platt est un sergent de bureau sergent un brain sarcastique du District 21. Elle est le supérieur immédiat des agents de patrouille du district et n'hésite pas à leur rappeler. Avant de prendre un emploi de bureau, elle était un officier de patrouille en uniforme et a travaillé avec Antonio Dawson.")
+        system ("cls") # efface le terminal 
+
+
+    elif information_personage_chicago_pd == ("Sean Roman"): # information du personage Sean Roman
+        print ("\t Aucune information")
+         
+
+
+
+
+    elif information_personage_chicago_pd == ("Justin Voight"): # information du personage Justin Voight
+        print ("\t Aucune information")
+         
+
+
+
+
+    else:
+        print ("\t Aucune information") # reponse Aucune information
+        system ("cls") # efface le terminal 
+
+
+
+
+        # Retour
+
+    print("\t<personage_chicago_pd>: Veux tu relancer ? [ < o > / < n > ]")
+
+    print ()
+
+    choice = input("\t <personage_chicago_pd>: ")
+
+    for i in tqdm(range(1000)): # Range = durée de chargement
+            pass
+
+    if choice == 'o':
+
+        print ()
+
+        
+
+        main()
+
+    elif choice == 'n':
+
+        print ()
+
+        print("\t <personage_chicago_pd>: .")
+
+        print ()
+
+        system ("cls")
+
+        print("\t Merci")
+
+        print("\t A bientôt")
+
+        print ()
+
+
+main()
+    
+
+    
